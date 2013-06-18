d462e1ffa80371884a3fc2ffaab3bd99e959b163
kumapon <kuma-dev@gmo-eclab.com>
2013-06-18 14:40:10 +0900

    Update out_dynamodb.rb
    modified plugin to send two batch request, one per table instead of forking the request.

--- lib/fluent/plugin/out_dynamodb.rb
+++ lib/fluent/plugin/out_dynamodb.rb
@@ -19,12 +19,15 @@ module Fluent
     config_param :aws_key_id, :string, :default => nil
     config_param :aws_sec_key, :string, :default => nil
     config_param :proxy_uri, :string, :default => nil
-    config_param :dynamo_db_table, :string
     config_param :dynamo_db_endpoint, :string, :default => nil
     config_param :time_format, :string, :default => nil
     config_param :detach_process, :integer, :default => 2
-    config_param :dynamo_db_hash_key, :string, :default => nil
-    config_param :dynamo_db_range_key, :string, :default => nil
+    config_param :dynamo_db_numbers, :string
+    config_param :dynamo_db_sessions, :string
+    config_param :dynamo_db_numbers_hash_key, :string, :default => nil
+    config_param :dynamo_db_numbers_range_key, :string, :default => nil
+    config_param :dynamo_db_sessions_hash_key, :string, :default => nil
+    config_param :dynamo_db_sessions_range_key, :string, :default => nil
     config_param :dynamo_array_flattener, :string, :default => nil
 
     def configure(conf)
@@ -50,7 +53,8 @@ module Fluent
 
         begin
           restart_session(options)
-          valid_table(@dynamo_db_table)
+          valid_table(@dynamo_db_numbers)
+          valid_table(@dynamo_db_sessions)
         rescue ConfigError => e
           $log.fatal "ConfigError: Please check your configuration, then restart fluentd. '#{e}'"
           exit!
@@ -63,7 +67,8 @@ module Fluent
 
     def restart_session(options)
       config = AWS.config(options)
-      @batch = AWS::DynamoDB::BatchWrite.new(config)
+      @batch_numbers = AWS::DynamoDB::BatchWrite.new(config)
+      @batch_sessions = AWS::DynamoDB::BatchWrite.new(config)
       @dynamo_db = AWS::DynamoDB.new(options)
     end
 
@@ -72,8 +77,6 @@ module Fluent
       table.load_schema
       raise ConfigError, "Binary hash keys not supported" if table.range_key.type.to_s == 'binary'
       raise ConfigError, "Database range key must be an Integer, it will hold a timestamp" if table.range_key.type.to_s != 'number'
-      @hash_key_value = table.hash_key.name
-      @hash_range_value = table.range_key.name
     end
 
     def format(tag, time, record)
@@ -81,58 +84,60 @@ module Fluent
       record.to_msgpack
     end
 
-    #Since we want to have a different behaviour for two tables from the same request write method gets a little bit messy
     def write(chunk)
-      batch_size = 0
-      batch_records = []
-      chunk.msgpack_each {|record|
-        next if record["table"] == "conversion" #Skipping conversion records, those should be handled by another request
-        if @dynamo_db_table.include?("number") #dirty dirty dirty, shame on me!
+      batch_number_records = []
+      batch_session_records = []
+      chunk.msgpack_each{|record|
+        next if record["table"] == "conversion"
+        begin
+          parsed_time = record["parsed_time"]
+          parsed_time ||= Time.now.to_s #Just in case
+
           unique_numbers = CGI.unescape(record['phone_numbers']).split(';').uniq rescue []
           unique_numbers.each do |number|
             #Skip bad records
             next unless (number != "" && number != nil)
-            next unless (record[@hash_range_value]!= "" && record[@hash_range_value]!= nil)
-            batch_records << [:number => number.gsub('-', '').gsub(' ', ''),
-                              :timestamp => record['timestamp'],
-                              :session_id => record['session_id']]
-          end
-        else
-          #Skip bad records
-          next unless (record[@hash_key_value]!= "" && record[@hash_key_value]!= nil)
-          next unless (record[@hash_range_value]!= "" && record[@hash_range_value]!= nil)
-          batch_records << build_session_object(record) rescue puts "Somewhing went wrong creating the session object"
-        end
+            #Create Number Record
+            batch_number_records << {:number => number.gsub('-', '').gsub(' ', ''),
+                                     :timestamp => Time.parse(parsed_time).to_i,
+                                     :session_id => record['session_id']}
+            #Create Session Record
+            batch_session_records << build_session_object(record, parsed_time)
 
-        batch_size += record.to_json.length # FIXME: heuristic
-        if batch_records.size >= BATCHWRITE_ITEM_LIMIT || batch_size >= BATCHWRITE_CONTENT_SIZE_LIMIT
-          batch_put_records(batch_records)
-          batch_records.clear
-          batch_size = 0
+            #Check if we have enough to push
+            if batch_number_records.size >= BATCHWRITE_ITEM_LIMIT or batch_session_records.size >= BATCHWRITE_ITEM_LIMIT
+              batch_put_records(batch_number_records, batch_session_records)
+              batch_session_records.clear
+              batch_number_records.clear
+            end
+          end
+        rescue Exception => e
+          $log.fatal("Somewhing went wrong, error: #{e.message}")
+          raise e
         end
       }
-      unless batch_records.empty?
-        batch_put_records(batch_records)
-      end
+      batch_put_records(batch_number_records, batch_session_records) if (batch_session_records.any? or batch_number_records.any?)
+      batch_session_records.clear
+      batch_number_records.clear
     end
 
-    def batch_put_records(records)
-      @batch.put(@dynamo_db_table, records.uniq)
-      @batch.process!
+    def batch_put_records(numbers, sessions)
+      @batch_numbers.put(@dynamo_db_numbers, numbers.uniq)
+      @batch_numbers.process!
+      @batch_sessions.put(@dynamo_db_sessions, sessions.uniq)
+      @batch_sessions.process!
     end
 
     def default_null(hash, key)
       (hash[key] == nil || hash[key] == '')  ? "null" : hash[key]
     end
 
-  def build_session_object(record)
-      parsed_time = record["parsed_time"]
-      parsed_time ||= Time.now.to_s #Just in case
+    def build_session_object(record, parsed_time)
       date_time = DateTime.parse(parsed_time)
-      date_time ||= DateTime.now #Just in case
+
       {
        'page_id' => default_null(record, 'landing_page_id').to_s,
-       'timestamp' => date_time.to_time.to_i,
+       'timestamp' => Time.parse(parsed_time).to_i,
        'session_id' => default_null(record, 'session_id'),
        'merchant_id' => default_null(record, 'merchant_id'),
        'program_id' => default_null(record, 'program_id'),
